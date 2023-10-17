3093b9caf3b45f15f96f47fcd1526a406724dcaa
Dinith Dharshika <dharshikadinith@gmail.com>
2023-10-17 15:52:16 +0530

    add testApi

---
+++ src/main/java/com/example/testApi/controller/apiController.java
@@ -0,0 +1,19 @@
+package com.example.testApi.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class apiController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(apiController.class);
+
+    @PostMapping(value = "/save", produces = "application/json")
+    public void logRequest(@RequestBody String requestBody) {
+        LOGGER.info("Received API Request Body: {}", requestBody);
+    }
+}
